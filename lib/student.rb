f1cd22ec064c5ac0be2330da366a9dbc8a8d1c27
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-21 17:55:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ def self.create_table
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
   
-  def self.create(hash) 
+  def self.create(student) 
     student = Student.new(:name, :grade)
     student.save
     student 
